66cdedac339bb1e29769b666330196742de62f57
Henryforce <hj_serrano@hotmail.com>
2016-12-31 12:03:35 -0600

    Updated project schemes

---
+++ Carthage/Build/Mac/KRActivityIndicatorView.framework/Versions/A/Headers/KRActivityIndicatorView.h
@@ -0,0 +1,19 @@
+//
+//  KRActivityIndicatorView.h
+//  KRActivityIndicatorView
+//
+//  Created by Henry Serrano on 12/31/16.
+//  Copyright © 2016 Henry Serrano. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for KRActivityIndicatorView.
+FOUNDATION_EXPORT double KRActivityIndicatorViewVersionNumber;
+
+//! Project version string for KRActivityIndicatorView.
+FOUNDATION_EXPORT const unsigned char KRActivityIndicatorViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KRActivityIndicatorView/PublicHeader.h>
+
+
