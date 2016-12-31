4aac77327d7347e37f27e28074fc49e3cd2fd419
Henryforce <hj_serrano@hotmail.com>
2016-12-31 11:33:38 -0600

    Initial Commit

---
+++ KRActivityIndicatorViewDemo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  KRActivityIndicatorViewDemo
+//
+//  Created by Henry Serrano on 12/31/16.
+//  Copyright © 2016 Henry Serrano. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ KRActivityIndicatorViewDemo/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  KRActivityIndicatorViewDemo
+//
+//  Created by Henry Serrano on 12/31/16.
+//  Copyright © 2016 Henry Serrano. All rights reserved.
+//
+
+import Cocoa
+import KRActivityIndicatorView
+
+class ViewController: NSViewController {
+
+    @IBOutlet var enclosureView : KEnclosureView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
